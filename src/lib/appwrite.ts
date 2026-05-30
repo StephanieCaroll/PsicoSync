5f588674473a92be04787d18fc2c806a74db9247
Stephanie <stephaniecaroline2006@gmail.com>
2026-05-30 02:57:34 -0300

    appwrite mudado

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -2,11 +2,18 @@ import { Client, Account, Databases } from 'appwrite';
 
 const client = new Client();
 
-client
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
+const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
+
+if (endpoint && projectId) {
+  client
+    .setEndpoint(endpoint)
+    .setProject(projectId);
+} else {
+  console.warn("Variáveis do Appwrite não encontradas. Verifique seu arquivo .env.local");
+}
 
 export const account = new Account(client);
-export const databases = new Databases(client); 
+export const databases = new Databases(client);
 
 export default client;
